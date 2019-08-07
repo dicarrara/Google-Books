f98433e3f8e2a5cca4c26d4c850d57155850ecec
Diana Carrara <dianaVcarrara@gmail.com>
2019-08-07 12:36:05 -0600

    add App.js logic

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require ("mongoose");
+const routes= require ("./routes")
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes
+app.use(routes)
+
+//Connet to mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {useNewUrlParser: true});
+
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
